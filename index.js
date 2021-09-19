f93e505d384695a0e76c0628b832a94f207c0e01
Hayeong Park <hayeongpark@naver.com>
2021-09-19 23:22:56 +0900

    Initial Commmit

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const hostname = '127.0.0.1'; //local host {localhost:3000도 가능}
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
